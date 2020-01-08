67951afd2962c166c52bc0def3b2a8be99395189
Alex80260 <alecouflet@la-providence.net>
2020-01-08 16:36:44 +0100

    git

--- index.php
+++ index.php
@@ -2,20 +2,32 @@
     <head>
 <title>Bienvenue sur SkoolMeat</title>
         <link rel="stylesheet" href="styleindex.css" />
-        
+        <nav>
+	        <a href="index.php">Accueil</a>
+            <a href="seconnecter.php">Connexion</a>
+            <a href="signin.php">Inscription</a>
+	        <div class="animation start-home"></div>
+        </nav>
     </head>
     <body>
-    <img class="logo" src="logo.PNG">
     
-    <a href="seconnecter.php">
-        <input class="button" type="button" value="Se connecter" />
-    </a>  
-    <a href="signin.php">
-        <input class="button" type="button" value="S'inscrire" />
-    </a>
-    <h2>SkoolMeat</h2>
+    <div>
+        <h1>Bienvenue sur SkoolMeat<h1>
+    <div>
+    <div class="presentation">
+        <div>
+            <p>SkoolMeat est une application d'aide et de recontre par du soutient scolaire</p>
+        </div>
+        <div>
+            <img class="logo" src="logo.PNG">
+        </div>
+            <p>Le but est de par vos centres d'intérêt et vos compétences scolaire de discuter et vous aidez</p>
+        <div>
 
+        </div>
+    </div>
     <img class="lapro" src="logo_providence.jpg">
+    <p><h3>Fondé par Theo Garnon et Alexis Lecouflet</h3></p>
 
     </body>
 </html>
